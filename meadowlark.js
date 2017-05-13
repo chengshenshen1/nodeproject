66b04f958bf39592271278b698c330f42b686a09
chengshenshen1 <535653568@qq.com>
2017-05-13 21:38:01 +0800

    node

---
+++ meadowlark.js
@@ -0,0 +1,42 @@
+var express = require('express');
+
+var app = express();
+
+//设置handlebars试图引擎
+var handlebars = require('express3-handlebars')
+    .create({default:'main'});
+app.engine('handlebars',handlebars.engine);
+app.set('view engine','handlebars');
+
+//设置端口
+app.set('port',process.env.PORT || 3000);
+
+// 设置static中间件
+app.use(express.static(__dirname + '/public'));
+
+//home 页面
+app.get('/',function (req,res) {
+    res.render('home');
+});
+
+//about 页面
+app.get('/about',function (req,res) {
+    res.render('about');
+});
+
+//404 catch-all 处理器 （中间件）
+app.use(function (req,res,next) {
+    res.status(404);
+    res.render('404')
+});
+
+//500错误处理器
+app.use(function (err,req,res,next) {
+    console.error(err.stack);
+    res.status(500);
+    res.render('500')
+});
+
+app.listen(app.get('port'),function () {
+    console.log('Express started on http://localhost:'+ app.get('post')+';press Crtl-C to terminate.')
+})
