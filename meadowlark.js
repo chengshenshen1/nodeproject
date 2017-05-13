5d969da5637ae77ef5ea7193f106af118b0f28b0
chengshenshen1 <535653568@qq.com>
2017-05-14 07:21:48 +0800

    添加幸运幸运饼干“
    gi commit -m 添加幸运幸运饼干“

--- meadowlark.js
+++ meadowlark.js
@@ -2,9 +2,17 @@ var express = require('express');
 
 var app = express();
 
+//幸运饼干数组
+var fortunes =[
+    "Conquer your fears or they will conquer you.",
+    "Rivers need springs.",
+    "Do not fear what you don't know",
+    "You will have a pleasant surprise",
+    "Whenever possible,keep it simple"
+];
 //设置handlebars试图引擎
 var handlebars = require('express3-handlebars')
-    .create({default:'main'});
+    .create({defaultLayout:'main'});
 app.engine('handlebars',handlebars.engine);
 app.set('view engine','handlebars');
 
@@ -21,7 +29,9 @@ app.get('/',function (req,res) {
 
 //about 页面
 app.get('/about',function (req,res) {
-    res.render('about');
+    var randomFortune =
+            fortunes[Math.floor(Math.random()*fortunes.length)];
+    res.render('about',{fortune:randomFortune});
 });
 
 //404 catch-all 处理器 （中间件）
@@ -39,4 +49,4 @@ app.use(function (err,req,res,next) {
 
 app.listen(app.get('port'),function () {
     console.log('Express started on http://localhost:'+ app.get('post')+';press Crtl-C to terminate.')
-})
+});
